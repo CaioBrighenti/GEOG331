a8a39b2e601d8cf1a65759b178aad5f0924eda54
Caio Brighenti <caio.brig@gmail.com>
2020-01-24 12:04:24 -0500

    add mathematical operations

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,3 +1,29 @@
 # Activity 1
 print("hello R world")
   
+# try out operations
+4*4
+
+4^2
+
+10 + 5 * 5
+
+# try variables
+a <- 15
+
+a^2
+
+a + a + 10
+
+# try vectors
+b <- c(10,50,125,500)
+
+b + a
+
+b / 10
+
+c <- c(a,a*2,a*3,a*4)
+
+c * 2
+
+c + b
