3e5ad398378ea5978154aa4982b3ee485a71e130
Caio Brighenti <caio.brig@gmail.com>
2020-03-01 13:53:02 -0500

    finish activity 55

--- activity5/activity5_script.R
+++ activity5/activity5_script.R
@@ -39,8 +39,7 @@ datD$hour <- hour(timesD ) + (minute(timesD )/60)
 #get full decimal time
 datD$decDay <- datD$doy + (datD$hour/24)
 #calculate a decimal year, but account for leap year
-datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/366),
-                       datD$year + (datD$decDay/365))
+datD$decYear <- datD$year + (datD$doy-1)/365
 #calculate times for datP                       
 datP$hour <- hour(dateP ) + (minute(dateP )/60)
 #get full decimal time
@@ -125,3 +124,141 @@ legend("topright", c("mean","1 standard deviation","2017"), #legend items
        col=c("black",rgb(0.392, 0.584, 0.929,.2),"red"),#colors
        pch=c(NA,15,NA),#symbols
        bty="n")#no legend border
+
+
+##### what days have full precip
+datP2 <- datP %>%
+        group_by(doy, year) %>%
+        add_count() %>%
+        mutate(fullPrecip = n >= 24)
+
+fullDays <- datP2 %>%
+        select(doy,year,fullPrecip) %>%
+        distinct()
+
+datD2 <- left_join(datD,fullDays)
+
+# make plot
+par(mai=c(1,1,1,1))
+plot(datD2$decYear, datD2$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+points(filter(datD2,fullPrecip)$decYear,filter(datD2,fullPrecip)$discharge,col="red",pch=20)
+legend("topright", c("full precip. available"), #legend items
+       lwd=c(NA),#lines
+       col=c("red"),#colors
+       pch=c(20),#symbols
+       bty="n")#no legend border
+
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+
+min(hydroD$discharge)
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+
+## look for winter days with full precip
+datD2 %>% filter(fullPrecip,year==2012) %>% arrange(-doy)
+## 2012, doy = 362 looks like good option
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 361 & datD$doy < 363 & datD$year == 2012,]
+hydroP <- datP[datP$doy >= 361 & datP$doy < 363 & datP$year == 2012,]
+
+min(hydroD$discharge)
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_boxplot()
+
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_violin()
+
+## seasons plot
+### WINTER - DEC. 1ST TO FEBRUARY 28TH
+### SPRING - MARCH 1ST TO MAY 31ST
+### SUMMER - JUNE 1ST TO AUGUST 31ST
+### AUTUMN - SEPTEMBER 1ST TO NOVEMBER 30TH
+library(tidyr)
+datD3 <- datD2 %>%
+        separate(date,into=c("month","day","year"),sep="/") %>%
+        mutate(season = case_when(month %in% c("12","1","2") ~ "WINTER",
+                                  month %in% c("3","4","5") ~ "SPRING",
+                                  month %in% c("6","7","8") ~ "SUMMER",
+                                  month %in% c("9","10","11") ~ "AUTUMN"
+                
+        ))
+
+datD3 %>%
+        filter(year %in% c(2016,2017)) %>%
+        ggplot(aes(x=season,y=discharge)) +
+        geom_violin(aes(fill=season,color=season)) +
+        facet_wrap(~year) +
+        theme_grey() +
+        labs(x = "Season",
+             y = expression(paste("Discharge ft"^"3 ","sec"^"-1"))
+        )
+
